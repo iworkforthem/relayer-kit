5e3f4fb40e1e3c566f817e39f98f6f5aee17737a
Chris Min <chris@dharma.io>
2018-09-11 18:15:06 -0700

    feat: update tutorial

--- dharma-retrieve-and-fill-loans.js
+++ dharma-retrieve-and-fill-loans.js
@@ -2,6 +2,7 @@
 const fetch = require("node-fetch");
 const Web3 = require("web3");
 const { Dharma } = require("@dharmaprotocol/dharma.js");
+const { LoanRequest, Token } = Dharma.Types;
 
 // Constants
 const CREDITOR_ADDRESS = "0x3FA17C1F1a0Ae2DB269F0B572ca44B15Bc83929a";
@@ -12,26 +13,30 @@ const ENDPOINT = "/v0/debt_orders";
 const provider = new Web3.providers.HttpProvider("http://localhost:8545");
 const dharma = new Dharma(provider);
 
-// Import the LoanRequest and Token Types
-const { LoanRequest, Token } = Dharma.Types;
+// Helper function to get the first LoanRequest from a Standard Relayer API
+async function getFirstLoanRequest(relayerApiHost, path) {
+    try {
+        const response = await fetch(`${relayerApiHost}${path}`);
 
-// Helper function to retrieve Debt Orders from a Standard Relayer API
-function retrieveDebtOrders() {
-    return new Promise((resolve, reject) => {
-        fetch(`${RELAYER_API_URL}${ENDPOINT}`)
-            .then((response) => resolve(response.json()))
-            .catch((reason) => reject(reason));
-    });
+        const responseJson = await response.json();
+
+        // Get the first Debt Order
+        const debtOrders = responseJson.debtOrders;
+
+        const debtOrder = debtOrders[0].debtOrder;
+
+        // Instantiate a Loan Request object using the Debt Order, so it is easier to interact with
+        const loanRequest = await LoanRequest.load(dharma, debtOrder);
+
+        return loanRequest;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
-retrieveDebtOrders().then(async (response) => {
-    // Get the first Debt Order
-    const debtOrders = response.debtOrders;
-
-    const debtOrder = debtOrders[0].debtOrder;
-
-    // Instantiate a Loan Request object using the Debt Order, so it is easier to interact with
-    const loanRequest = await LoanRequest.load(dharma, debtOrder);
+// Function to fill the first LoanRequest from a Standard Relayer API
+async function fillFirstLoanRequest(relayerApiHost, path) {
+    const loanRequest = await getFirstLoanRequest(relayerApiHost, path);
 
     // Set the creditor allowance so she may fill the Loan Request
     const loanTerms = loanRequest.getTerms();
@@ -55,4 +60,6 @@ retrieveDebtOrders().then(async (response) => {
     await dharma.blockchain.awaitTransactionMinedAsync(fillTransactionHash);
 
     process.exit();
-});
+}
+
+fillFirstLoanRequest(RELAYER_API_URL, ENDPOINT);
